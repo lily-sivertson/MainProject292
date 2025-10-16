0cc40589cbcd20cb42299ee62561cd1c3bacd607
lsiverts <Lilysivertson05@gmail.com>
2025-10-16 18:34:59 -0400

    input actions
    input actions are not working

--- myTImeAtStardew/Assets/Scripts/Player.cs
+++ myTImeAtStardew/Assets/Scripts/Player.cs
@@ -9,19 +9,27 @@ public class Player : MonoBehaviour
     [SerializeField] InputActionAsset playerActions;
 
     InputAction move;
+    InputAction jump;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         move = playerActions.FindActionMap("Keyboard").FindAction("Move");
+        jump= playerActions.FindActionMap("Keyboard").FindAction("Jump");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveValue = move.ReadValue<Vector2>();
-        Debug.Log(moveValue);
+        Vector2 moveValue = move.ReadValue<Vector2>(); // supposed to read the movement input and tell which key is being pressed.
+        //Debug.Log(moveValue);
+
+        if (jump.IsPressed())
+        {
+            Debug.Log("Jump");
+        }
+        
     }
 
 
